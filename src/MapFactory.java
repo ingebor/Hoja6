a93f65a9a4ff9f021161bf09ab9adeea4b5ebf4b
Julio Herrera <jurhs2000@gmail.com>
2020-03-11 13:21:33 -0600

    factory llamando clases

--- src/MapFactory.java
+++ src/MapFactory.java
@@ -1,5 +1,14 @@
 class MapFactory<K, V> {
     public Map<K, V> getMap(int mapType) {
-        return null;
+        switch (mapType) {
+            case 1:
+                return new HashMap<K,V>();
+            case 2:
+                return new TreeMap<K,V>();
+            case 3:
+                return new LinkedHashMap<K,V>();
+            default:
+                return null;
+        }
     }
 }
